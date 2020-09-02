5e43eccb9592d4e764938732b017399d324f2d67
cijee <saykuray@foxmail.com>
2020-09-02 18:58:10 +0800

    Initial commit

---
+++ gmall-coupon/src/test/java/top/cijee/gmall/coupon/GmallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package top.cijee.gmall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gmall-product/src/test/java/top/cijee/gmail/product/GmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package top.cijee.gmail.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
